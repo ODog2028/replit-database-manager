1d140e070675c8dbc5aacb7e276200017d0e678e
Owen Mielcarek <96429650+ODog2028@users.noreply.github.com>
2022-02-12 21:11:32 +0000

    Added Code to manage.py and finished README.md

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # replit-database-manager
 Database Manager for Replit!
+# Contents
+  - [Start Maninaging databases!](#start-managing-databases!)
+  - [Commands](#commands)  
 # Start Managing databases!
 To Start Managing databases, go to https://repl.it/ and run create a new repl for python.
 <br>
@@ -8,3 +11,24 @@ Go to the Shell tab and type <br>
 <br> To start the manager, Go to the Shell tab and type <br>
 `cd replit-database-manager` <br>
 `python manage.py`
+# Commands
+This python code is it's own Command Line Interface based on CLI! All of the commands are case sensitive.<br>
+The Commands are:<br>
+  - [Add](#add)
+  - [Set](#add)
+  - [Value](#add) 
+  - [Delete](#add) 
+  - [List](#add) 
+  - [Search](#add)
+## Add 
+The command `Add` adds a key to the replit database with a value.
+## Set 
+The command `Set` modifies a specific key in the replit database.
+## Value 
+The command `Value` reads a value of a specific key.
+## Delete 
+The command `Delete` removes a specific key from the replit database with confirmation.
+## List
+The command `List` lists all the keys in the replit database without value.
+## Search 
+The command `Search` takes in the first couple of letters and searches in the replit database without value.
---
+++ manage.py
@@ -0,0 +1,89 @@
+# Name: replit-database-manager
+# Owner: Owen Mielcarek (odog2028 on github)
+# Description: Database Manager for Replit!
+
+# Import Needed Libraries
+from replit import db
+import time
+import os
+
+
+# Again Function
+def again():
+    again = input("Would you like to run more commands? (y or n)")
+    if again == "y":
+        print("Okay!")
+        time.sleep(1)
+        os.system('clear')
+        manager()
+    elif again == "n":
+        print("Okay!")
+        time.sleep(1)
+        print("Bye!")
+    else:
+        print("Sorry, I didn't get that!")
+        time.sleep(1)
+        again = input("Would you like to run more commands? (y or n)")
+        if again == "y":
+            print("Okay!")
+            time.sleep(1)
+            os.system('clear')
+            manager()
+        elif again == "n":
+            print("Okay!")
+            time.sleep(1)
+            print("Bye!")
+        else:
+            print("Sorry, I didn't get that!")
+            time.sleep(1)
+            print("Bye!")
+
+
+# Manager Function
+def manager():
+    command = input("Please enter command:\n")
+    if command == "Set" or command == "Add":
+        new_key = input("Please enter key!\n")
+        new_key_value = input("Please enter value of the new key!\n")
+        db[new_key] = new_key_value
+    elif command == "Value":
+        key = input("Please enter key!\n")
+        value = str(db[key])
+        print("The Value of the key is '" + value + "'")
+    elif command == "Delete":
+        del_key = input("Please enter the key you want to delete!\n")
+        sure = input("Are you sure (y or n)")
+        if sure == "y":
+            try:
+                del db[del_key]
+                print("Deleted!")
+            except:
+                print("Their was an error! Please Try Again")
+                time.sleep(1)
+                del_key = input("Please enter the key you want to delete!\n")
+                sure = input("Are you sure (y or n)")
+                if sure == "y":
+                    try:
+                        del db[del_key]
+                        print("Deleted!")
+                    except:
+                        print("Their was an error!")
+                elif sure == "n":
+                    print("Okay!")
+        elif sure == "n":
+            print("Okay!")
+    elif command == "List":
+        keys = str(db.keys())
+        print("All of you keys are\n" + keys)
+    elif command == "Search":
+        keyword = input(
+            "What key are you looking for (The beginning of the key only!!)?\n"
+        )
+        result = str(db.prefix(keyword))
+        print("This is what I found: " + result)
+    else:
+        print("That is not a command. Please try again")
+    again()
+
+
+manager()
